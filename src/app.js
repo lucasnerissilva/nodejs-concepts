e27e1f28bcbda3d47d41068548a3ff872bee2ba6
Lucas Neris <lucas.neris@globalpoints.com.br>
2020-09-02 20:02:30 -0300

    uuid check middleware was created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,20 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+const checkUUID = () => {
+  return (req, res, next) => {
+    const { id } = req.params;
+
+    if (!isUuid(id))
+      return res.status(400).send({
+        success: false,
+        error: "invalid UUID type"
+      });
+
+    return next();
+  };
+};
 
 app.get("/repositories", (request, response) => {
   // TODO
@@ -18,15 +32,15 @@ app.post("/repositories", (request, response) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", checkUUID(), (request, response) => {
   // TODO
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", checkUUID(), (request, response) => {
   // TODO
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", checkUUID(), (request, response) => {
   // TODO
 });
 
